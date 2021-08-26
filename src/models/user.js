43e5577cd6a4028b9cbced9b62dc51d5160aeb44
Antovero <dosaeun@gmail.com>
2021-08-27 01:06:58 +0900

    error fix

--- src/models/user.js
+++ src/models/user.js
@@ -8,7 +8,6 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String },
     name: { type: String, required: true },
-    // location: String,
     comments: [
         { type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
     ],
