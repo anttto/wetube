21821b42a3bea5e1c3f0351111967ed408dade75
Antovero <dosaeun@gmail.com>
2021-08-27 13:53:32 +0900

    error fix v1.1

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,7 +7,7 @@ import {
     startGithubLogin, finishGithubLogin,
     getChangePassword, postChangePassword
 } from "../controllers/userController"
-import { protectorMiddleware, publicOnlyMiddleware, avatarUpload, s3DeleteAvatarMiddleware } from "../middlewares"
+import { protectorMiddleware, publicOnlyMiddleware, avatarUpload } from "../middlewares"
 
 
 const userRouter = express.Router();
@@ -17,7 +17,7 @@ userRouter
     .route('/edit')
     .all(protectorMiddleware)
     .get(getEdit)
-    .post(s3DeleteAvatarMiddleware, avatarUpload.single("avatar"), postEdit);
+    .post(avatarUpload.single("avatar"), postEdit);
 userRouter.route('/change-password').all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get('/github/start', publicOnlyMiddleware, startGithubLogin);
 userRouter.get('/github/finish', publicOnlyMiddleware, finishGithubLogin);
