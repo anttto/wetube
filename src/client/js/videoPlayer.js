6b91b25a87ce7577d15416ce1f68c84b8a1d5c6a
Antovero <dosaeun@gmail.com>
2021-08-29 00:45:04 +0900

    v.3.2

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -65,8 +65,9 @@ const handleVolumeChange = (event) => {
 const formatTime = (seconds) =>
   new Date(seconds * 1000).toISOString().substr(14, 5);
 
-const handleLoadedMetadata = async () => {
-  totalTime.innerText = await formatTime(Math.floor(video.duration));
+const handleLoadedMetadata = () => {
+  console.log(video.duration);
+  totalTime.innerText = formatTime(Math.floor(video.duration));
   timeline.max = Math.floor(video.duration);
 };
 
